8e77632920c456265cb84de722e3de9ed10eddb8
RAJNEESH RAI <146312745+therajneesh1@users.noreply.github.com>
2024-09-02 17:10:52 +0530

    Add files via upload

---
+++ CALCULATOR/calculator.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearEntry() {
+  display.value = '';
+}
+
+function clearDisplay() {
+    display.value = '';
+  }
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
